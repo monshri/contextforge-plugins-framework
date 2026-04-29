8628926e9c3fcdae5860c8d9cf20142f39307b19
Shriti Priya <shritip@ibm.com>
2026-04-29 10:58:14 -0400

    Initial allow deny sandboxing
    Signed-off-by: Shriti Priya <shritip@ibm.com>

--- sandboxing/src/main.rs
+++ sandboxing/src/main.rs
@@ -1,6 +1,9 @@
+
+// Potential Sandbox Manager
 use wasmtime::component::*;
 use wasmtime::{Config, Engine, Store};
 use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiView};
+use wasmtime_wasi::{DirPerms, FilePerms};
 
 wasmtime::component::bindgen!({
     path: "plugin/wit",
@@ -23,6 +26,8 @@ impl WasiView for MyState {
     }
 }
 
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = Config::new();
     config.wasm_component_model(true);
@@ -31,17 +36,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut linker = Linker::new(&engine);
     wasmtime_wasi::add_to_linker_sync(&mut linker)?;
     
-    let wasi = WasiCtxBuilder::new().inherit_stdio().build();
+    // let wasi = WasiCtxBuilder::new().inherit_stdio().preopened_dir("./host-directory", ".", DirPerms::all(), FilePerms::all());
+    let wasi = WasiCtxBuilder::new().inherit_stdio().preopened_dir("./data", ".", DirPerms::all(), FilePerms::all())?.build();
+
     let state = MyState { 
         wasi,
         table: ResourceTable::new(),
     };
     let mut store = Store::new(&engine, state);
     
-    let component = Component::from_file(&engine, "plugin/target/wasm32-wasip1/release/plugin.wasm")?;
+    let component = Component::from_file(&engine, "plugin/target/wasm32-wasip2/release/plugin.wasm")?;
     let plugin = Plugin::instantiate(&mut store, &component, &linker)?;
     
-    let json = r#"{"status": "please deny this"}"#;
+    let json = r#"{"status": "please allow this"}"#;
     let result = plugin.example_plugin_policy().call_check_key(&mut store, json, "status")?;
     
     println!("Result: {}", result);
